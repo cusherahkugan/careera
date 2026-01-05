7696ca679781741871491331e433174935f1fd91
cusherah <cusherahkugan@gmail.com>
2026-01-05 11:15:33 +0530

    prisma setup done

--- prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "@prisma/cli";
-
-export default defineConfig({
-  client: {
-    adapter: "postgresql",          // your database type
-    url: process.env.DATABASE_URL,  // Prisma reads this from .env.local
-  },
-});
